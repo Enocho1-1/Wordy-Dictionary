19f1c9d567459569da31d711622dc94ef4f4058e
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-06-18 21:33:51 -0400

    Update FavoriteList.js

--- wordy/src/components/FavoriteList.js
+++ wordy/src/components/FavoriteList.js
@@ -1,10 +1,15 @@
-
+import star from "../assests/star.png"
 
 export const FavoriteList = ({favoriteList}) => {
   return (
-    <div className=" flex flex-col justify-centerw-[650px] p-6">
-        <h1 className="text-3xl text-coral font-bold my-3 text-center" id="favoriteTitle">Favorites</h1>
-        <aside className="mt-4 h-[150px] p-2 w-[450px] flex justify-center flex-wrap overflow-y-scroll">
+    <div className=" flex flex-col justify-center w-[650px] p-6">
+        <header className="flex justify-center">
+            <div className="flex">
+              <img src={star} className="h-6 self-center mx-4" alt="" />
+                <h1 className="font-sans font-extrabold text-4xl text-coral" id="favoriteTitle">Favorites</h1>
+            </div>
+        </header>
+        <aside className="mt-4 h-[150px] p-2 w-fit flex justify-center flex-wrap overflow-y-scroll">
             {favoriteList.map(item => (
                 <div key={item.id} className="flex flex-col justify-center items-center h-[100px] w-[150px] mx-3 my-4">
                     <p className="text-xl text-coral font-semibold">{item.word}</p>
