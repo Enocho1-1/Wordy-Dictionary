18c3dfe7fe37efd3a7be89ccce409156894f9441
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-06-23 12:56:28 -0400

    19 commit
    mobile responsive design search component

--- wordy/src/pages/Search.js
+++ wordy/src/pages/Search.js
@@ -35,12 +35,12 @@ export const Search = ({apiPath, favoriteList, setFavorites}) => {
 
   return (
     <>
-        <section className="absolute z-10 h-full max-w-7xl flex justify-center items-center">
+        <section className="absolute z-10 h-full w-full flex justify-center items-center">
           {/* Found/Not Found conditional rendering */}
           { def.length === 0 
             ?< NotFound/>
             :  
-              <div className=" relative p-6">
+              <div className=" relative p-6 max-sm:w-[350px]">
                 <ToastContainer
                     position="top-left"
                     autoClose={1000}
@@ -55,20 +55,21 @@ export const Search = ({apiPath, favoriteList, setFavorites}) => {
                     />
                 {/* Favorite Word Button */}
                 <span onClick={() => {handleFavoriteWord() ; notify()}} className=" absolute top-5 right-5 hover:cursor-pointer" title="favorite"><img className="animate-bounce h-10 w-10" src={star} alt="" /></span>
+
                 {/* User Word & Part of Speech */}
                 <div className="flex flex-col">
-                  <aside className="flex items-center">
-                    <h1 className="text-7xl font-extrabold">{word}</h1>
-                    <p className="text-5xl text-coral font-bold self-end my-[2px] mx-4">{pos}</p>
+                  <aside className="flex max-sm:flex-col ">
+                    <h1 className="text-7xl font-extrabold max-sm:text-5xl">{word}</h1>
+                    <p className="text-5xl max-sm:text-4xl text-coral font-bold self-end max-sm:self-start max-sm:mx-1 my-[2px] mx-4">{pos}</p>
                   </aside>
-                  <aside className="flex mt-2">
+                  <aside className="flex max-sm:flex-col mt-2">
                     <p className="mt-4 text-4xl mx-2">{pronounce}</p>
-                    <button onClick={audioPlay} type="button" className="flex items-center text-lightRed border border-lightRed hover:bg-red-200 focus:outline-none focus:ring-red-300 font-medium rounded-[45px] text-md mt-4 px-2 text-center align-self-center">{aword}<img className="h-6 mx-2" src={audioImg} alt="audio"/></button>
+                    <button onClick={audioPlay} type="button" className="flex items-center text-lightRed border border-lightRed hover:bg-red-200 focus:outline-none focus:ring-red-300 font-medium rounded-[45px] text-md max-sm:text-xl max-sm:py-1 mt-4 px-2 text-center align-self-center max-sm:w-fit">{aword}<img className="h-6 mx-2" src={audioImg} alt="audio"/></button>
                   </aside>
                 </div>
 
                 {/* Word Variants List */}
-                <div className="mt-2 flex flex-wrap">
+                <div className="mt-2 flex flex-wrap max-sm:flex-col">
                   {wordArray.map((item, index) => (
                     <p className="text-2xl text-olive font-bold mx-1" key={index}>{item}</p>
                   ))}
@@ -78,8 +79,8 @@ export const Search = ({apiPath, favoriteList, setFavorites}) => {
                 <div className="mt-4">
                   <ol>
                     {def.map((item, index ) => (
-                      <div key={index}  className=""> 
-                        <li className="text-3xl font-semibold my-3" ><span className="text-2xl text-lime mx-3">{index}:</span>{item}</li>
+                      <div key={index}> 
+                        <li className="text-3xl font-semibold my-3" ><span className="text-2xl text-lime mr-1">{index}:</span>{item}</li>
                       </div>
                     
                     ))}
