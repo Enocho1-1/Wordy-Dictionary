0245701fc5d30dad79ca53319ebd5b116c5c001a
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-06-23 13:40:44 -0400

    22 commit
    reponsive design

--- wordy/src/App.js
+++ wordy/src/App.js
@@ -5,11 +5,11 @@ import videoBg from "./assests/videoBg.mp4"
 
 function App() {
   return (
-    <main className="relative w-full h-screen flex justify-center items-center">
-       <header className="z-20 absolute top-[12%] max-sm:top-[5%] max-[375px]:top-[3%]">
+    <main className="relative w-full h-screen flex justify-center">
+       <header className="z-20 absolute w-fit top-[12%] max-sm:top-[5%] max-[360px]:top-[10%] max-[375px]:top-[3%] min-[390px]:max-[414px]:top-[10%]">
             <div className="flex">
                 <Link to="/"><img src={Logo} className="h-12 max-sm:h-9 self-center mx-4" alt="" /></Link>
-                <h1 className="font-sans font-extrabold text-7xl max-sm:text-4xl text-coral" id="homeTitle">Wordy Dictionary</h1>
+                <h1 className="font-sans font-extrabold text-7xl max-[375px]:text-4xl max-sm:text-5xl text-coral" id="homeTitle">Wordy Dictionary</h1>
             </div>
         </header>
       <div className="absolute top-0 left-0 h-full w-full z-10" id="overlay"></div>
--- wordy/src/pages/NotFound.js
+++ wordy/src/pages/NotFound.js
@@ -6,8 +6,8 @@ export const NotFound = () => {
     <section className="max-w-7xl ">
         <div className="p-6">
             <span className="flex justify-center ">
-                <h1 className="text-center font-extrabold text-5xl my-6 text-lightRed" id="notFound">Word Not Found</h1>
-                <img className="self-center mx-4 h-12 w-16" src={sadFace} alt="" />
+                <h1 className="text-center font-extrabold text-5xl my-6 max-sm:text-4xl text-lightRed" id="notFound">Word Not Found</h1>
+                <img className="self-center mx-4 h-12 w-16 max-sm:w-10" src={sadFace} alt="" />
             </span>
             <img className="rounded-md" src={sad} alt="" />
         </div>
